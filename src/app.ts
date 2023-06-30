926b11ccee6a729088d8ebbea5e5a7e5ffd25b22
Pali Frotan <pali.frotan@icloud.com>
2023-06-30 22:10:37 +0100

    Feat:Refactored code separated concerns

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 import express from 'express';
-export const app = express();
+import { router } from './routes/routes';
 
+export const app = express();
 app.get('/', (req, res) => res.send('Welcome to the Drinks API!'));
 app.get('/coffeelover', (req, res) => res.send('I like coffee!'));
 app.use(express.json());
-app.get('/coffee', (req, res) => res.json({
-    drinkType: 'Coffee',
-    name: 'Latte',
-}));
+app.use('/', router);
